c4cffe036632d4615fe68f0e3486ea73fef449af
Damien Nelson <dhnisaman@gmail.com>
2023-07-31 00:22:11 -0400

    commit tree 0f0e41de786ebd352bd6f13a63020a22c5461776

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,22 @@
-let state;
+function createStore(reducer){
+  let state;
 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState(){
+    return state
+  }
+
+  return { 
+    dispatch,
+    getState,
+  }
+}
+
+//  we have access to the state because these functions are used by dispatch and dispatch is defined in the same scope as the state variable. 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case "counter/increment":
@@ -10,19 +27,29 @@ function reducer(state = { count: 0 }, action) {
   }
 }
 
-function dispatch(action) {
-  state = reducer(state, action);
-  render();
-}
-
 function render() {
   let container = document.getElementById("container");
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 }
 
-dispatch({ type: "@@INIT" });
+// here we set the variable store so we can now gain access to the return value of the functions defined in the createStore function
+let store = createStore(reducer)
+
+store.dispatch({ type: "@@INIT" });
 let button = document.getElementById("button");
 
 button.addEventListener("click", function () {
-  dispatch({ type: "counter/increment" });
+  store.dispatch({ type: "counter/increment" });
 });
+
+// over all this reminds me of OOP essentially to get this to work se go through some steps inside the createStore function
+
+// 1. we initialize a variable but we dont have a setter or getter to use them so we have to create them
+
+// 2. we difine the dispatch method witch makes it able to access the state variable in the functions scope. this will allow the functions called by dispatch to have access to the state variable.
+
+// 3. we create a getter function to get the value of the state after dispatch is ran
+
+// 4. diffrent form OOP we need to return the functions that we defined to gain access to the return values or the side effects. so when i call dispatch it will call render wich will be a side effect of calling dispatch. and when we call get state we will get the value returned form withing the funciton.
+
+// 
